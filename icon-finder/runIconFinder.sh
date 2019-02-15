58d7f17525b98776dcb1cdb3ca421d4941d02f91
Joel Weiser <joel.weiser@oicr.on.ca>
2019-02-15 13:14:29 -0500

    Runs the configuration script if there is no custom config file

--- icon-finder/runIconFinder.sh
+++ icon-finder/runIconFinder.sh
@@ -9,18 +9,25 @@ if [ -z $input_file ]; then
     exit 1;
 fi
 
+## If the input file does not exist, print an error and exit
 if [ ! -f $input_file ]; then
     echo "$input_file not found"
     exit 1;
 fi
 
-## Creates symbolic link to user specified input file so it can be found by the default configuration
-ln -sf $(readlink -f $input_file) src/main/resources/input.txt
-
 ## Make sure the repo is up to date
 git pull
+
+config_file=config.properties
+## If the config file does not exist, run configuration script
+if [ -z $config_file ]; then
+    ./configureIconFinder.sh
+fi
+
+## Creates symbolic link to user specified input file so it can be found by the default configuration
+ln -sf $(readlink -f $input_file) src/main/resources/input.txt
 
 ## Generate the jar file and run the Icon Finder program
 mvn clean package
 ln -sf target/icon-finder-1.0-SNAPSHOT-jar-with-dependencies.jar icon-finder.jar
-java -jar icon-finder.jar > entities_in_diagrams.txt
+java -jar icon-finder.jar $config_file > entities_in_diagrams.txt
