99c24042de1eaa9219537707bd71d657fac26d60
Joel Weiser <joel.weiser@oicr.on.ca>
2019-02-15 11:31:44 -0500

    Requires an input file as the first argument

--- icon-finder/runIconFinder.sh
+++ icon-finder/runIconFinder.sh
@@ -2,6 +2,20 @@
 DIR=$(dirname "$(readlink -f "$0")") # Directory of the script -- allows the script to invoked from anywhere
 cd $DIR
 
+input_file=$1
+## If an input file is not provided, print an error and exit
+if [-z $input_file]; then
+    echo "Usage: ./$0 [input_file]"
+    exit 1;
+fi
+
+if [! -f $input_file]; then
+    echo "$input_file not found"
+    exit 1;
+fi
+
+ln -sf $input_file src/main/resources/input.txt
+
 ## Make sure the repo is up to date
 git pull
 
